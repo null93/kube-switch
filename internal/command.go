aba9a2c07c62c4f49b55516cbd619108ea48dd69
Rafael Grigorian <me@raffi.io>
2021-03-28 01:43:09 -0500

    Released Version 2.0.2

--- internal/command.go
+++ internal/command.go
@@ -9,7 +9,7 @@ import (
 
 var command = &cobra.Command {
 	Use: "kube-switch",
-	Version: "2.0.1",
+	Version: "2.0.2",
 	Short: "Switch between Kubernetes context & namespace using an interactive menu",
 	Example: strings.Join ( [] string {
 		"  kube-switch",
