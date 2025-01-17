d95d8f458bccbf0e7cc4feff7224994d883beb7c
Rafael Grigorian <rafael@grigorian.org>
2025-01-16 22:43:51 -0600

    Changed picker tui

--- internal/main.go
+++ internal/main.go
@@ -4,7 +4,7 @@ import (
 	"os"
 	"fmt"
 	"github.com/null93/kube-switch/sdk/k8s"
-	"github.com/null93/kube-switch/sdk/prompt"
+	"github.com/null93/aws-knox/sdk/picker"
 )
 
 func FatalError ( message string ) {
@@ -18,17 +18,21 @@ func PickContextAndNamespace ( useCurrentContext bool ) {
 		FatalError ("Error: Could not load configured contexts.")
 	}
 	if !useCurrentContext {
-		pickedContext, exited := prompt.Pick ( prompt.Options {
-			Choices: contexts,
-			DefaultValue: currentContext,
-			Header: "Pick Kubernetes Context",
-			SelectionPrefix: "",
-		})
-		if exited {
+		p := picker.NewPicker()
+		p.WithMaxHeight(10)
+		p.WithFilterStrategy("fuzzy")
+		p.WithEmptyMessage("No Kubernetes Contexts Found")
+		p.WithTitle("Pick Kubernetes Context")
+		p.WithHeaders("CONTEXT")
+		for _, name := range contexts {
+			p.AddOption(name, name)
+		}
+		selection, _ := p.Pick("")
+		if selection == nil {
 			return
 		}
-		currentContext = pickedContext
-		if error := k8s.ChangeContext ( pickedContext ); error != nil {
+		currentContext = selection.Value.(string)
+		if error := k8s.ChangeContext ( currentContext ); error != nil {
 			FatalError ("Error: Failed to write current context to config.")
 		}
 	}
@@ -36,13 +40,21 @@ func PickContextAndNamespace ( useCurrentContext bool ) {
 	if error != nil {
 		FatalError ("Error: Failed to load list of namespaces in current context.")
 	}
-	pickedNamespace, _ := prompt.Pick ( prompt.Options {
-		Choices: namespaces,
-		DefaultValue: currentNamespace,
-		Header: "Pick Kubernetes Context",
-		SelectionPrefix: currentContext + " > ",
-	})
-	if error := k8s.ChangeNamespace ( currentContext, pickedNamespace ); error != nil {
+	p := picker.NewPicker()
+	p.WithMaxHeight(10)
+	p.WithFilterStrategy("fuzzy")
+	p.WithEmptyMessage("No Kubernetes Namespaces Found")
+	p.WithTitle("Pick Kubernetes Namespace")
+	p.WithHeaders("NAMESPACE")
+	for _, name := range namespaces {
+		p.AddOption(name, name)
+	}
+	selection, _ := p.Pick("")
+	if selection == nil {
+		return
+	}
+	currentNamespace = selection.Value.(string)
+	if error := k8s.ChangeNamespace ( currentContext, currentNamespace ); error != nil {
 		FatalError ("Error: Failed to write current namespace to config.")
 	}
 }
